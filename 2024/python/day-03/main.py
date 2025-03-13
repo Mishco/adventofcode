9ac12e8e85e36ea165efc6a9350f148ccbc805af
Michal Slovik <michal.slovik@tutanota.com>
2025-03-13 13:39:32 +0100

    Add day 03 first part

--- 2024/python/day-03/main.py
+++ 2024/python/day-03/main.py
@@ -1,10 +1,25 @@
+import re
+
 
 def part1():
-    pass
+    pattern = r"mul\((\d+),\s*(\d+)\)"
+    total = 0
+    with open('../../rust/data/inputs/03.txt', mode='r') as f:
+        lines = f.read().splitlines()
+
+        for line in lines:
+            for item in re.finditer(pattern, line):
+                first = int(item[1])
+                second = int(item[2])
+
+                total += (first * second)
+    return total
+
 
 def part2():
     pass
 
+
 if __name__ == '__main__':
-    part1()
-    part2()
+    print(part1())
+    print(part2())
